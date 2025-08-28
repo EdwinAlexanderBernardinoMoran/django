22da1a0474fee09983b79c228fde9264032763cc
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-08-28 11:43:20 -0600

    S1 - Simplificando logica de forma dinamica

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:day>/', views.days_weeks_numbers, name='day_number'),
     path('<str:day>/', views.days_weeks, name='day')
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+days_of_week = {
+    'monday': "Start your week with positivity!",
+    'tuesday': "Keep going, it's only Tuesday!",
+    'wednesday': "You're halfway through the week!",
+    'thursday': "Almost there, it's Thursday!",
+    'friday': "Finish strong, it's Friday!"
+}
 
 # Create your views here.
 
-def days_weeks(request, day):
-    quote_text = ''
-    if day == 'monday':
-        quote_text = "Start your week with positivity!"
-    elif day == 'tuesday':
-        quote_text = "Keep going, it's only Tuesday!"
-    elif day == 'wednesday':
-        quote_text = "You're halfway through the week!"
-    elif day == 'thursday':
-        quote_text = "Almost there, it's Thursday!"
-    elif day == 'friday':
-        quote_text = "Finish strong, it's Friday!"
-    else:
-        return HttpResponseNotFound("Invalid day!")
+def days_weeks_numbers(request, day):
+    days = list(days_of_week.keys())
 
-    return HttpResponse(quote_text)
+    if day > len(days):
+        return HttpResponseNotFound("Invalid day!")
+    
+    redirect_day = days[day-1]
+    return HttpResponseRedirect(f"/quotes/{redirect_day}")
+
+def days_weeks(request, day):
+    try:
+        return HttpResponse(days_of_week[day])
+    except KeyError:
+        return HttpResponseNotFound("Invalid day!")
