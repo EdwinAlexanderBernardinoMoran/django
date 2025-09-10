c70b382e7fbee6221e5c0ed428ad1b728d5ff2d0
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-09-10 10:10:04 -0600

    S5 - Archivos estaticos globales

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Añadimos la ruta de los archivos estáticos globales
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
